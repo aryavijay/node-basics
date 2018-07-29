7e54ff4152ab349e5fafbfa9a2874d9ed4ee1b67
Vijay Kumar <vijayarya326@gmail.com>
2018-07-29 19:39:30 +0530

    adding function to custom lib

--- notes.js
+++ notes.js
@@ -2,12 +2,20 @@ console.log("Starting Notes App");
 
 const fs = require('fs');
 const os = require('os');
+const notesObj = require('./lib/notes-function.js');
 
 var user = os.userInfo();
 
-fs.appendFile("note.txt", `Hello ${user.username}, How are you. New Text. \r\n`, (err)=>{
-  if(err){
-  console.log("Error Appending file");
-  console.log(err);
-  }
-});
+function writefile(){
+  fs.appendFile("note.txt", `Hello ${user.username}, How are you. You are ${notesObj.age} yrs of age. \r\n`, (err)=>{
+    if(err){
+    console.log("Error Appending file");
+    console.log(err);
+    }
+  });
+}
+
+notesObj.addNote();
+console.log(notesObj.addFun(3,4));
+
+//writefile();
