f0205c099c1f9538b1a5cf70809e343dc1f0abf1
Vijay Kumar <vijayarya326@gmail.com>
2018-07-29 18:30:11 +0530

    Appending Text to file

---
+++ notes.js
@@ -0,0 +1,11 @@
+console.log("Starting Notes App");
+
+const fs = require('fs');
+
+fs.appendFile("note.txt", "How are you. New Text. \r\n", (err)=>{
+  if(err){
+  console.log("Error Appending file");
+  console.log(err);
+  }
+
+});
