b59d6ad860e96b9d998ef72c292a4daa6c3e9ada
Vijay Kumar <vijayarya326@gmail.com>
2018-07-29 18:43:50 +0530

    adding greeting with logged in system

--- notes.js
+++ notes.js
@@ -1,8 +1,11 @@
 console.log("Starting Notes App");
 
 const fs = require('fs');
+const os = require('os');
 
-fs.appendFile("note.txt", "How are you. New Text. \r\n", (err)=>{
+var user = os.userInfo();
+
+fs.appendFile("note.txt", `Hello ${user.username}, How are you. New Text. \r\n`, (err)=>{
   if(err){
   console.log("Error Appending file");
   console.log(err);
