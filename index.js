c98cdea78e979c59134c463d76e84dafc02be892
Vijay Kumar <vijayarya326@gmail.com>
2018-07-29 17:59:33 +0530

    First Commit

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-var name = "Vijay";
+console.log("How are you");
+console.log("Factorial");
 
-console.log(name);
+number = 5;
+for (i = number; i > 1; i--) {
+    number = number * (i - 1);
+}
+console.log(number);
+
+/*console.log("Looop")
+for(i =1; i<= 10; i++){
+    setTimeout(()=>{console.log(i)}, 1000);
+}*/
+
+var interval = setInterval(() => {
+    console.log("vijay")
+}, 3000);
+
+setTimeout(() => {
+    clearInterval(interval);
+}, 15000);
